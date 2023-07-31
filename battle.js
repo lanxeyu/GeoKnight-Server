2a3664f39e222fa714cff800ba8b1de535247b5c
lanxeyu <lanxeyu@gmail.com>
2023-07-31 16:26:21 +0100

    Add basic combat functionality

--- battle.js
+++ battle.js
@@ -1,34 +1,113 @@
-/* PSEUDOCODE
 
-connect to score API
+// Initialize difficulty scalers
+let battleCounter = 0;
+let enemyStatScaler = battleCounter * 1.1;
+let enemyCountScaler = battleCounter * 1.5;
 
-cycle counter
+// Initialize score
+let score = 0;
 
-create player object
-    declare its properties / stats
-    apply changes to the stats based on Fixing phase
-create enemy object/s
-    declare its properties / stats
-    apply changes to the stats based on how many cycles has happened
-
-create enemyArray containing all enemy objects
-
-Event listener for the attack button
-if attack btn is pressed
-    hit()
-        player.currHP -= enemy.atk
-        enemyArray[0].currHP -= player.atk
-        
-    checkHP()
-        if this.currHP == 0
-        destroy object
-
-    combatLog()
-        create textcontent = You dealt x damage to enemy!
-        You took 5 damage...
-        Monster died!
-        etc...
-
-*/
+// Initialize player object
+const player = {
+    maxHP: 10,
+    currHP: 10,
+    attack: 2,
+    
+    displayStats() {
+      console.log(`Player - Max HP: ${this.maxHP}, Current HP: ${this.currHP}, Attack: ${this.attack}`);
+    }
+};
 
 
+// Create empty array to store enemies
+let enemyArray = [];
+
+
+// Class constructor for enemy objects
+class Enemy {
+    constructor(maxHP, attack) {
+      this.maxHP = maxHP;
+      this.currHP = maxHP;
+      this.attack = attack;
+    }
+
+    displayStats() {
+        console.log(
+          `${this.name} - Health: ${this.health}, Attack: ${this.attack}`
+        );
+    }
+}
+
+
+// Create and add an enemy to the enemyArray
+function createEnemy(maxHP, attack) {
+    const newEnemy = new Enemy(maxHP, attack);
+    enemyArray.push(newEnemy);
+    // return newEnemy; // Optional return, might need later
+}
+
+
+// Increase number of enemies and enemy stats based on battleCounter 
+function adjustDifficulty() {
+    enemyStatScaler = battleCounter * 1.1;
+    enemyCountScaler = battleCounter * 1.5;
+    enemyMaxHPMidpoint = xxx;
+    enemyAtkMidpoint = xxx;
+}
+
+
+function initBattlePhase() {
+    adjustDifficulty();
+    // Increment battleCounter, used to increase difficulty as more cycles occur
+    battleCounter += 1;
+
+    // INSERT HERE: function to draw the battle phase screen
+   
+    
+    // Create enemies dynamically based on difficulty
+}
+
+
+function resolveAttack(player, enemy) {
+    player.currHP -= enemyArray[0].atk
+    enemyArray[0].currHP -= player.atk
+}
+
+
+function checkIfZeroHP(object) {
+    if (object.currHP <= 0) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+
+function runBattlePhase() {
+    // Event listener for the attack button
+    document.getElementById("attack-btn").addEventListener("click", () => {
+        // If attack button is pressed, do the following
+        resolveAttack();
+
+        if (checkIfZeroHP(player)) {
+            player = null;
+
+            // INSERT HERE: Go to "Losing/Score Display/Enter Your Name" screen
+        }
+
+        if (checkIfZeroHP(enemyArray[0])) {
+            // Remove enemy object from the enemyArray
+            enemyArray.shift();
+
+            // If there are more enemies
+            if (enemyArray.length <= 0) {
+            
+            
+            // INSERT HERE: Successful defense message. Then go to Fixing phase.
+            }
+        }
+    });
+}
+
+
+module.exports = {battle}
